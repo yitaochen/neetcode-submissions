adbaeebc3a25803691450c0d168422b5fcc990a1
Yitao Chen <yitaochen@utexas.edu>
2026-06-14 11:37:20 -0700

    Add: longest-increasing-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # the O(n^2) solution 
+        n = len(nums)
+        dp = [1] * n
+        for i in range(n):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[j] + 1, dp[i])
+        
+        return max(dp)
